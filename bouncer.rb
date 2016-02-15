46b5ae155dcd652c76d9be0609c5f5334a7dc854
“Zaki <reach.zaki.ahmed@gmail.com>
2016-02-15 12:03:10 -0600

    bouncerhw

---
+++ bouncer.rb
@@ -0,0 +1,21 @@
+def show_menu
+  puts "============================"
+  puts "|Are you an Old Fart or Not|"
+  puts "============================"
+end
+puts show_menu
+
+def check_age
+  puts "How Old Are You?"
+  choice=gets.chomp.to_i
+  if choice < 18
+    return "I can do Nothing"
+  elsif choice >=18 && choice <21
+    return "I can Vote & I can Smoke "
+  elsif choice >= 21 && choice <25
+    return "I can Drink"
+  elsif choice >=25
+    return "I can rent a Car"
+end
+end
+puts check_age
